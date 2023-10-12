4ee1ac029c92271cc82791945479c8ae9e21d381
YukiNoMercy <matveyschenikov@mail.ru>
2023-10-12 23:59:02 +0300

    function sum of 2 numbers

--- task.go
+++ task.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	a += b
+	return a
+}
 func main() {
-
-	fmt.Println("hello world")
+	a := 10
+	b := 15
+	res := sum(a, b)
+	fmt.Println(a, " + ", b, " = ", res)
 }
