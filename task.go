ead2c51b43cd5ea157edb5373f6063643223278b
YukiNoMercy <matveyschenikov@mail.ru>
2023-10-13 00:41:20 +0300

    add function division 2 numbers

--- task.go
+++ task.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func division(a float64, b float64) float64 {
+	a /= b
+	return a
+}
 func multiplication(a int, b int) int {
 	a *= b
 	return a
@@ -17,10 +21,14 @@ func sum(a int, b int) int {
 func main() {
 	a := 10
 	b := 15
+	div_a := 10.0
+	div_b := 15.0
+	div := division(div_a, div_b)
 	summa := sum(a, b)
 	sub := subtraction(a, b)
 	multi := multiplication(a, b)
 	fmt.Println(a, " + ", b, " = ", summa)
 	fmt.Println(a, " - ", b, " = ", sub)
 	fmt.Println(a, " * ", b, " = ", multi)
+	fmt.Println(div_a, " / ", div_b, " = ", div)
 }
